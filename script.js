93ccb136027b580fb6e30405036df29774e3db7e
Francisamos123 <sugartone45@yahoo.com>
2023-05-23 22:59:36 +0000

    commit to main

---
+++ script.js
@@ -0,0 +1,11 @@
+let show = document.getElementById("nav-links")
+  
+function showMenu(){
+   show.style.right = "0";
+   console.log('clicked')
+}
+
+function closeMenu(){
+   show.style.right = "-200px";
+   console.log('clicked me')
+}
